c45cd323c0a982162cd502b78c099a9bb7ef2cd2
Bryan Tor <bterrific2008@gmail.com>
2021-07-22 12:48:25 -0700

    Use wrapper functions

--- scrape.py
+++ scrape.py
@@ -1,38 +1,71 @@
-import praw
-from praw.models.reddit.submission import Submission
-from praw.models.reddit.comment import Comment
-from praw.models.reddit.subreddit import Subreddit
-
 import os
 from datetime import datetime
+
+import praw
+import pymongo
 import pytz
+from praw.models.reddit.comment import Comment
+from praw.models.reddit.submission import Submission
+from praw.models.reddit.subreddit import Subreddit
 
-CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
-CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
-USER_AGENT = os.environ['REDDIT_USER_AGENT']
+from wrappers.wrappers import SubmissionCollections, CommentCollections
 
-TARGET_SUBREDDIT = os.environ['REDDIT_TARGET_SUBREDDIT']
-SCRAPE_POST_COUNT = int(os.environ['REDDIT_SCRAPE_POST_COUNT'])
-SCRAPE_COMMENT_COUNT = int(os.environ['REDDIT_SCRAPE_COMMENT_COUNT'])
+CLIENT_ID = os.environ["REDDIT_CLIENT_ID"]
+CLIENT_SECRET = os.environ["REDDIT_CLIENT_SECRET"]
+USER_AGENT = os.environ["REDDIT_USER_AGENT"]
+
+TARGET_SUBREDDIT = os.environ["REDDIT_TARGET_SUBREDDIT"]
+SCRAPE_POST_COUNT = int(os.environ["REDDIT_SCRAPE_POST_COUNT"])
+SCRAPE_COMMENT_COUNT = int(os.environ["REDDIT_SCRAPE_COMMENT_COUNT"])
+
+MONGO_HOST = os.environ["MONGO_HOST"]
+MONGO_PORT = int(os.environ["MONGO_PORT"])
+DATABASE_NAME = os.environ["MONGO_DB_NAME"]
+POST_COLLECTION_NAME = os.environ["POST_COLLECTION_NAME"]
+COMMENT_COLLECTION_NAME = os.environ["COMMENT_COLLECTION_NAME"]
+
+
+def convert_post_to_document(post: Submission):
+    post_doc = {
+        "_id": post.id,
+        "title": post.title,
+        "author": {"id": post.author.id, "name": post.author.name},
+        "subreddit": post.subreddit.display_name,
+        "text": post.selftext,
+        "date": datetime.datetime.fromtimestamp(post.created),
+    }
+
+    return post_doc
+
 
 def main():
 
-
+    mongodb = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
+    collection = mongodb[DATABASE_NAME][POST_COLLECTION_NAME]
+    reddit = praw.Reddit(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        user_agent=USER_AGENT
+    )
     subreddit: Subreddit = praw.Reddit(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         user_agent=USER_AGENT,
     ).subreddit(TARGET_SUBREDDIT)
 
+    post_collection = SubmissionCollections(collection=collection, reddit=reddit)
+    comment_collection = CommentCollections(collection=collection, reddit=reddit)
+
     new_posts = subreddit.new(limit=SCRAPE_POST_COUNT)
     post: Submission
     for post in new_posts:
-        print(post.title, post.created_utc)
+        post_collection.insert_one(post)
 
     new_comments = subreddit.comments(limit=SCRAPE_COMMENT_COUNT)
     comment: Comment
     for comment in new_comments:
-        print(comment.author, comment.body[:50], comment.created_utc)
+        comment_collection.insert_one(comment)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
